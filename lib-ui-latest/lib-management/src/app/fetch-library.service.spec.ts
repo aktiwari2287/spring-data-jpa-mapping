b6c8e5ab18f6fd72d372ca089c50112ddecbafed
aktiwari <aktiwari@nextgen.com>
2021-02-15 09:35:02 +0530

    Added testcases for fetch all libraries and fetch all books for a library

--- lib-ui-latest/lib-management/src/app/fetch-library.service.spec.ts
+++ lib-ui-latest/lib-management/src/app/fetch-library.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { FetchLibraryService } from './fetch-library.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 import { Book } from './book';
+import { Library } from './library';
 describe('FetchLibraryService', () => {
   let service: FetchLibraryService,
       httpTestingController: HttpTestingController;
@@ -50,4 +51,45 @@ describe('FetchLibraryService', () => {
 
   });
  
+  it('should fetch all the Libraries', ()=>{
+    const dummyLibraries:Library[] = [{
+                                        "lib_id":"1",
+                                        "name":"html"
+                                      }
+                                  ];
+    service.getAllLibraries().subscribe(libraries=>{
+      expect(libraries.length).toBe(1);
+      expect(libraries).toEqual(dummyLibraries);;
+    })
+
+    const request = httpTestingController.expectOne(service.ROOT_URL+'libraries/all');
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyLibraries);
+  });
+
+  it('should fetch all the books for a given Library', ()=>{
+      const dummyBooks: Book[] = [
+        {
+          "book_id": "100",
+          "isbn": "12345",
+          "title": "GoLang",
+          "cover": "GoLang2",
+          "publisher": "Anand kumar",
+          "pages": "500",
+          "available": "105",
+          "library": {
+            "name": "GoLang"
+          }
+        }
+    ];
+
+    service.getAllBooksForLibrary('1').subscribe(books=>{
+      expect(books.length).toBe(1);
+      expect(books).toEqual(dummyBooks);
+    });
+
+    const request = httpTestingController.expectOne(service.ROOT_URL+`books/all/1`);
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyBooks);
+    })
 });
